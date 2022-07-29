a80a25af4706275e181bc87c31b7e223f3b384b9
Sagar k.c <sagarkc45172@gmail.com>
2022-07-29 23:19:27 +0545

    final commit of the day

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -23,6 +23,58 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(
               color: Colors.grey, fontSize: 24, fontWeight: FontWeight.bold),
         ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.favorite_outline_rounded,
+              size: 37,
+              color: Colors.grey,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.messenger_outline,
+              size: 37,
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(
+              icon: Icon(
+                Icons.home,
+                size: 37,
+              ),
+              label: ""),
+          NavigationDestination(
+              icon: Icon(
+                Icons.search,
+                size: 37,
+              ),
+              label: ""),
+          NavigationDestination(
+              icon: Icon(
+                Icons.add,
+                size: 37,
+              ),
+              label: ""),
+          NavigationDestination(
+              icon: Icon(
+                Icons.shop,
+                size: 37,
+              ),
+              label: ""),
+          NavigationDestination(
+              icon: Icon(
+                Icons.circle,
+                size: 37,
+              ),
+              label: ""),
+        ],
       ),
     );
   }
