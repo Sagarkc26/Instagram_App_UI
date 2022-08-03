cf03a869119b044f9d189cf21926b15929bd076c
Sagar k.c <sagarkc45172@gmail.com>
2022-08-03 23:38:25 +0545

    Adder multiple story in story widget

--- lib/widget/story.dart
+++ lib/widget/story.dart
@@ -5,36 +5,44 @@ class StoryWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        width: 67,
-        height: 67,
-        decoration: const BoxDecoration(
-          shape: BoxShape.circle,
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color(0xFF982282),
-              Color(0xFFEEA863),
-            ],
+    return Column(
+      children: [
+        Row(
+          children: List.generate(
+            4,
+            (index) {
+              return Container(
+                width: 67,
+                height: 67,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0xFF982282),
+                      Color(0xFFEEA863),
+                    ],
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 64,
+                    height: 64,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage("images/heart.png"),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
           ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            width: 64,
-            height: 64,
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                image: AssetImage("images/heart.png"),
-              ),
-            ),
-          ),
-        ),
-      ),
+        )
+      ],
     );
   }
 }
